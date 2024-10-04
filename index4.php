581716572d01d01e9755dd7459c8f2f4a4ffd8de
Nikolay Chelpanov <nikch3008@mail.ru>
2024-10-04 18:14:41 +0700

    upd

--- index4.php
+++ index4.php
@@ -1,43 +1,49 @@
 <?php
-function liter($str){
-	$array = array(	
-		"а" => "a", 
-		"б" => "b", 
-		"в" => "v", 
-		"г" => "g", 
-		"д" => "d", 
-		"е" => "e", 
-		"ё" => "yo",
-		"ж" => "zh", 
-		"з" => "z", 
-		"и" => "i", 
-		"й" => "y", 
-		"к" => "k", 
-		"л" => "l", 
-		"м" => "m",
-		"н" => "n", 
-		"о" => "o", 
-		"п" => "p", 
-		"р" => "r", 
-		"с" => "s", 
-		"т" => "t", 
-		"у" => "u",
-		"ф" => "f", 
-		"х" => "h", 
-		"ц" => "ts", 
-		"ч" => "ch", 
-		"ш" => "sh", 
-		"щ" => "s'h", 
-		"ъ" => "",
-		"ы" => "i", 
-		"ь" => "'", 
-		"э" => "e", 
-		"ю" => "yu", 
-		"я" => "ya", 
-		" " => " "
-	);
-	$string = strtr($str, $array);
-	echo "$string \r\n";
+
+
+
+$string = "Кошка!";
+echo "Изначальная строка: " . $string . "\n";
+
+$alfabet = [
+    'а' => 'a', 'б' => 'b', 'в' => 'v',
+    'г' => 'g', 'д' => 'd', 'е' => 'e',
+    'ё' => 'e', 'ж' => 'zh', 'з' => 'z',
+    'и' => 'i', 'й' => 'y', 'к' => 'k',
+    'л' => 'l', 'м' => 'm', 'н' => 'n',
+    'о' => 'o', 'п' => 'p', 'р' => 'r',
+    'с' => 's', 'т' => 't', 'у' => 'u',
+    'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+    'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch',
+    'ь' => '\'', 'ы' => 'y', 'ъ' => '\'',
+    'э' => 'e', 'ю' => 'yu', 'я' => 'ya'
+];
+
+function translate($string, $alfabet) {
+    $result = "";
+    $length = mb_strlen($string);
+    
+    for ($i = 0; $i < $length; $i ++)
+    {
+    	$letter = mb_substr($string, $i, 1);
+    	
+    	if (isset($alfabet[mb_strtolower($letter)])) {
+    		if ($letter === mb_strtolower($letter)){
+    			$newLetter = $alfabet[$letter];
+    		}
+    		else{
+    			$newLetter = ucfirst($alfabet[mb_strtolower($letter)]);
+    		}
+    		
+    	}
+    	
+    	else{
+    		$newLetter = $letter;
+    	}
+    	$result .= $newLetter;
+    }
+    return $result;
 }
 
-liter("кошка");
+
+echo "Итоговая строка: " . translate($string, $alfabet);
